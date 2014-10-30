68abbd2159f846e3733bdea3577884c72d1a45be
Lucas Czech <luc@s-cze.ch>
2014-10-30 19:36:08 +0100

    added main.hh to provide namespace description to doxygen

---
+++ src/main/main.hh
@@ -0,0 +1,10 @@
+#ifndef GNS_MAIN_MAIN_H_
+#define GNS_MAIN_MAIN_H_
+
+/**
+ * @namespace genesis
+ * @brief This namespace contains all symbols of genesis in order to keep them
+ * separate when used as a library.
+ */
+
+#endif // include guard
