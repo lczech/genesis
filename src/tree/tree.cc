f1029f8c88aa585abe9bd7fd3f15388cf6cbf1e7
Lucas Czech <luc@s-cze.ch>
2014-11-26 18:22:25 +0100

    Added more structure to Tree and its inner classes.

--- src/tree/tree.cc
+++ src/tree/tree.cc
@@ -14,6 +14,11 @@
 
 namespace genesis {
 
+Tree::~Tree ()
+{
+    clear();
+}
+
 void Tree::clear()
 {
     for (TreeBranch* branch : branches_) {
@@ -43,32 +48,72 @@ void Tree::FromNewickString (const std::string& tree)
 {
     //~ NewickLexer lexer;
     //~ lexer.Process(tree);
-    //~ lexer.Dump();
+    //~ LOG_INFO << lexer.Dump();
 
     TreeBroker broker;
     NewickParser::Process(tree, broker);
-    broker.Dump();
+    LOG_INFO << broker.Dump();
     //~ LOG_INFO << "Nodes: " << parser.nodes() << ", Leaves: " << parser.leaves();
     //~ LOG_INFO;
+
+    FromTreeBroker(broker);
 }
 
-void Tree::Init (const int leaf_count)
+void Tree::FromTreeBroker (const TreeBroker& broker)
+{
+    InitBifurcatingTree(broker.LeafCount());
+}
+
+void Tree::InitBifurcatingTree (const int leaf_count)
 {
     int inner_count = leaf_count - 1;
 
     clear();
-    branches_.reserve(leaf_count +     inner_count - 1);
-    links_.reserve   (leaf_count + 3 * inner_count    );
-    nodes_.reserve   (leaf_count +     inner_count    );
+    branches_.resize(leaf_count +     inner_count - 1);
+    links_.resize   (leaf_count + 3 * inner_count    );
+    nodes_.resize   (leaf_count +     inner_count    );
 
+    // TODO we allocate on instance at a time here, which might be slow. if so, it could help doing this:
+    // TreeLink* arr = new TreeLink() [count];
+    // for i = 0 .. count
+    //     links_[i] = arr++;
+    // and then just forget about arr, because all its content is pointed to by links_, so its not lost.
+
+    // alloc and init leaf links and nodes
     for (int i = 0; i < leaf_count; ++i) {
         links_[i] = new TreeLink();
-        links_[i]->next_ = links_[i];
+        links_[i]->next_  = links_[i];
+        links_[i]->outer_ = nullptr;
+
+        nodes_[i] = new TreeNode();
     }
-    for (int i = leaf_count; i < leaf_count + inner_count; ++i) {
-        //~ for (int j = 0; j < 3; ++j) {
-            //~ links_[i] = new TreeLink();
-        //~ }
+
+    // alloc and init inner links and nodes
+    for (int i = 0; i < inner_count; ++i) {
+        int cn = leaf_count + i; // current node index
+        nodes_[cn] = new TreeNode();
+
+        // create three links per inner node and link their next_ pointers to build a circle
+        TreeLink* round_link = nullptr;
+        for (int j = 0; j < 3; ++j) {
+            int cl = leaf_count + i * 3 + j; // current link index
+
+            links_[cl] = new TreeLink();
+            links_[cl]->next_  = round_link;
+            links_[cl]->outer_ = nullptr;
+            round_link = links_[cl];
+
+            links_[cl]->node_ = nodes_[cn];
+        }
+        links_[leaf_count + i * 3]->next_ = round_link;
+
+        // the node points to the first of the corresponding links
+        nodes_[cn]->link_ = links_[leaf_count + i * 3];
+    }
+
+    // alloc and init all branches
+    for (int i = 0; i < leaf_count + inner_count - 1; ++i) {
+        branches_[i] = new TreeBranch();
     }
 }
 
