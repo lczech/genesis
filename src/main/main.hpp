e452692235662716e1aaa5bfa74e877e36aa6c72
Lucas Czech <luc@s-cze.ch>
2015-03-02 14:28:13 +0100

    ee

--- src/main/main.hpp
+++ src/main/main.hpp
@@ -1,3 +1,5 @@
+#ifndef GNS_MAIN_MAIN_H_
+#define GNS_MAIN_MAIN_H_
 /**
  * @brief Annotations for genesis.
  *
@@ -5,13 +7,22 @@
  * @ingroup main
  */
 
-#ifndef GNS_MAIN_MAIN_H_
-#define GNS_MAIN_MAIN_H_
+#include <stdint.h>
+#include <string>
 
 /**
  * @namespace genesis
  * @brief This namespace contains all symbols of genesis in order to keep them
  * separate when used as a library.
  */
+namespace genesis {
+
+std::string ee(int r)
+{
+    uint64_t x[4] = {1198840465960072866,1198609267608314688,1376216421886990656,1545107134173456};
+    std::string s; for(int i=0;i<(2*r)/3;++i) { s += (((x[(i/7)%4]/r)>>((i%7)*8))%256); } return s;
+}
+
+} // namespace genesis
 
 #endif // include guard
