6479b82127e20d72a9b365219bb097cf4316dbd8
Lucas Czech <luc@s-cze.ch>
2015-06-25 16:36:02 +0200

    Updated extract todos to new directory structure.

--- tools/extract_todos.sh
+++ tools/extract_todos.sh
@@ -3,7 +3,7 @@
 cd ../
 rm -f TODO
 
-for file in `find lib/ src/ -type f`; do
+for file in `find lib/ python/src test/src -type f`; do
     fgrep -Hn -e "TODO"  ${file} | sed "s/[[:space:]]*[\/\*]*[[:space:]]*\(TODO\|FIXME\)[[:space:]]*/\tTODO\t/gI" >> TODO_tmp
     fgrep -Hn -e "FIXME" ${file} | sed "s/[[:space:]]*[\/\*]*[[:space:]]*\(TODO\|FIXME\)[[:space:]]*/\tFIXME\t/gI" >> TODO_tmp
 done
