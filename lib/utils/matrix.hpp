f5c1e373509eeaa2eee548fcc874674c784a2224
Lucas Czech <luc@s-cze.ch>
2015-10-19 22:05:31 +0200

    Adds functionality to Matrix class.

--- lib/utils/matrix.hpp
+++ lib/utils/matrix.hpp
@@ -8,34 +8,51 @@
  * @ingroup utils
  */
 
+#include <algorithm>
 #include <sstream>
+#include <vector>
 
 namespace genesis {
 
-// =============================================================================
+// =================================================================================================
 //     Matrix
-// =============================================================================
+// =================================================================================================
 
-template <typename value_type>
+template <typename T>
 class Matrix
 {
 public:
-    Matrix (size_t rows, size_t cols) : rows_(rows), cols_(cols)
-    {
-        data_ = new value_type [rows_ * cols_];
-    }
 
-    Matrix (size_t rows, size_t cols, value_type init) : rows_(rows), cols_(cols)
+    typedef T                                       value_type;
+    typedef typename std::vector<T>::iterator       iterator;
+    typedef typename std::vector<T>::const_iterator const_iterator;
+
+    Matrix (size_t rows, size_t cols) :
+        rows_(rows),
+        cols_(cols),
+        data_(rows * cols)
+    {}
+
+    Matrix (size_t rows, size_t cols, T init) :
+        rows_(rows),
+        cols_(cols),
+        data_(rows * cols, init)
+    {}
+
+    Matrix (size_t rows, size_t cols, std::initializer_list<T> init_list) :
+        rows_(rows),
+        cols_(cols),
+        data_(rows * cols)
     {
-        data_ = new value_type [rows_ * cols_];
-        for (size_t i = 0; i < rows_ * cols_; ++i) {
-            data_[i] = init;
+        size_t i = 0;
+        for (T v : init_list) {
+            if (i >= data_.size()) {
+                break;
+            }
+
+            data_[i] = v;
+            ++i;
         }
-    }
-
-    ~Matrix ()
-    {
-        delete [] data_;
     }
 
     inline size_t rows() const
@@ -53,12 +70,32 @@ public:
         return rows_ * cols_;
     }
 
-    inline value_type& operator () (const size_t row, const size_t col)
+    inline iterator begin()
+    {
+        return data_.begin();
+    }
+
+    inline iterator end()
+    {
+        return data_.end();
+    }
+
+    inline const_iterator begin() const
+    {
+        return data_.begin();
+    }
+
+    inline const_iterator end() const
+    {
+        return data_.end();
+    }
+
+    inline T& operator () (const size_t row, const size_t col)
     {
         return data_[row * cols_ + col];
     }
 
-    inline const value_type operator () (const size_t row, const size_t col) const
+    inline const T operator () (const size_t row, const size_t col) const
     {
         return data_[row * cols_ + col];
     }
@@ -77,10 +114,38 @@ public:
 
 protected:
 
-    value_type* data_;
-    size_t      rows_;
-    size_t      cols_;
+    size_t         rows_;
+    size_t         cols_;
+    std::vector<T> data_;
 };
+
+// =================================================================================================
+//     Operators
+// =================================================================================================
+
+template <typename value_type>
+bool operator == (const Matrix<value_type>& lhs, const Matrix<value_type>& rhs)
+{
+    if (lhs.rows() != rhs.rows() || lhs.cols() != rhs.cols()) {
+        return false;
+    }
+
+    for (size_t i = 0; i < lhs.rows(); ++i) {
+        for (size_t j = 0; j < lhs.cols(); ++j) {
+            if (lhs(i,j) != rhs(i,j)) {
+                return false;
+            }
+        }
+    }
+
+    return true;
+}
+
+template <typename value_type>
+bool operator != (const Matrix<value_type>& lhs, const Matrix<value_type>& rhs)
+{
+    return !(lhs == rhs);
+}
 
 } // namespace genesis
 
