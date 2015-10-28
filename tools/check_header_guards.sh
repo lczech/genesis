cb8894ce84eecd1d5023a52f525e9f040ed0b300
Lucas Czech <luc@s-cze.ch>
2015-10-28 13:40:43 +0100

    Adds header guard check script.

---
+++ tools/check_header_guards.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# This script checks whether the include guards of the headers in ./lib are what they
+# should be according to their path and file name.
+
+cd ../lib/
+
+for header in `find . -name "*.hpp"`; do
+    guard="${header/.\//genesis_}"
+    guard="${guard/.hpp/_h_}"
+    guard="${guard//\//_}"
+    guard="${guard^^}"
+
+    line=`head -n 1 ${header} | cut -d " " -f 2`
+
+    if [[ ${line} != ${guard} ]]; then
+        echo "In ${header}:"
+        echo "   Actual: ${line}"
+        echo "   Wanted: ${guard}"
+    fi
+done
