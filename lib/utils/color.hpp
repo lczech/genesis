71404a35b29a844d8109b8e14cf3228b70ea22f2
Lucas Czech <luc@s-cze.ch>
2015-10-29 14:01:07 +0100

    Adds default constructor to Color.

--- lib/utils/color.hpp
+++ lib/utils/color.hpp
@@ -30,6 +30,8 @@ public:
     //     Constructors
     // -------------------------------------------------------------------------
 
+    Color () : r(0), g(0), b(0) {}
+
     Color (unsigned char r, unsigned char g, unsigned char b) : r(r), g(g), b(b) {}
 
     // Color (double r, double g, double b) :
