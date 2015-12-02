89a2f5c641a1d1771086eb7c6226305c3ba9aa42
Lucas Czech <luc@s-cze.ch>
2015-12-02 18:04:57 +0100

    Add Newick color mixin.

--- test/src/tree/newick.cpp
+++ test/src/tree/newick.cpp
@@ -11,11 +11,13 @@
 
 #include "lib/tree/default/newick_processor.hpp"
 #include "lib/tree/io/newick/processor.hpp"
+#include "lib/tree/io/newick/color_mixin.hpp"
 #include "lib/tree/tree.hpp"
+#include "lib/utils/string/string.hpp"
 
 using namespace genesis;
 
-TEST(NewickProcessor, FromAndToString)
+TEST(Newick, FromAndToString)
 {
     std::string input = "((A,(B,C)D)E,((F,(G,H)I)J,K)L)R;";
 
@@ -26,7 +28,7 @@ TEST(NewickProcessor, FromAndToString)
     EXPECT_EQ(input, output);
 }
 
-TEST(NewickProcessor, NewickVariants)
+TEST(Newick, NewickVariants)
 {
     DefaultTree tree;
 
@@ -92,4 +94,41 @@ TEST(NewickProcessor, NewickVariants)
         tree
     ));
     EXPECT_TRUE(tree.validate());
+}
+
+TEST(Newick, ColorMixin)
+{
+    std::string input = "((A,(B,C)D)E,((F,(G,H)I)J,K)L)R;";
+
+    DefaultTree tree;
+    typedef DefaultTreeNewickMixin<NewickColorMixin<NewickProcessor<DefaultTree>>> ColorTreeNewickProcessor;
+
+    // Make sure that the mixin does not interfere with other Newick functionality. If it does, the
+    // following line would hopefully crash.
+    EXPECT_TRUE( ColorTreeNewickProcessor().from_string(input, tree) );
+
+    // Create a color vector for all edges that marks edges leading to a leaf node in red.
+    auto color_vector = std::vector<Color>( tree.edge_count() );
+    for( auto it = tree.begin_edges(); it != tree.end_edges(); ++it ) {
+        if( (*it)->primary_node()->is_leaf() || (*it)->secondary_node()->is_leaf() ) {
+            color_vector[(*it)->index()] = Color(255, 0, 0);
+        }
+    }
+
+    // Use the color vector to produce a newick string with color tags.
+    // We set ignored color to fuchsia ("magic pink") in order to also print out the black colored
+    // inner edges.
+    auto proc = ColorTreeNewickProcessor();
+    proc.edge_color_vector(color_vector);
+    proc.ignored_color(Color(255, 0, 255));
+    std::string output = proc.to_string(tree);
+
+    // Check if we actually got the right number of red color tag comments.
+    auto count_red = string_count_substring_occurrences( output, "[&!color=#ff0000]" );
+    EXPECT_EQ( tree.leaf_count(), count_red );
+
+    // Check if we also got the right number of black color tag comments.
+    // This is one fewer than the number of nodes, as no color tag is written for the root.
+    auto count_black = string_count_substring_occurrences( output, "[&!color=#000000]" );
+    EXPECT_EQ( tree.inner_count() - 1, count_black );
 }
