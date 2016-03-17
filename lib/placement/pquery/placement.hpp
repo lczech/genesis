2b3a12e055f452d0c3391cababceb68b921d1084
Lucas Czech <lucas.czech@h-its.org>
2016-03-17 08:34:40 +0100

    Add Rule of Five to Placement class.

--- lib/placement/pquery/placement.hpp
+++ lib/placement/pquery/placement.hpp
@@ -54,34 +54,14 @@ public:
     //     Constructor and Rule of Five
     // -------------------------------------------------------------------
 
-    // TODO introduce rule of five, make awesome c++ 11 stuff!
-    // TODO make construction private and Pquery a friend who is the only one to construct a Placement.
-
-    PqueryPlacement () :
-        likelihood(0.0),
-        like_weight_ratio(0.0),
-        proximal_length(0.0),
-        pendant_length(0.0),
-        parsimony(0),
-        edge_(nullptr)
-    {}
-
-    /**
-     * @brief Something lika copy constructor. Takes the pquery and edge for inserting this
-     * placement, plus another placement, from which the other values are copied.
-     */
-    PqueryPlacement (
-        const PqueryPlacement& other
-    ) :
-        likelihood(other.likelihood),
-        like_weight_ratio(other.like_weight_ratio),
-        proximal_length(other.proximal_length),
-        pendant_length(other.pendant_length),
-        parsimony(other.parsimony),
-        edge_(nullptr)
-    {}
-
+    PqueryPlacement () = default;
     ~PqueryPlacement() = default;
+
+    PqueryPlacement( PqueryPlacement const& ) = default;
+    PqueryPlacement( PqueryPlacement&& )      = default;
+
+    PqueryPlacement& operator= ( PqueryPlacement const& ) = default;
+    PqueryPlacement& operator= ( PqueryPlacement&& )      = default;
 
     // -------------------------------------------------------------------
     //     Public Property Data Members
