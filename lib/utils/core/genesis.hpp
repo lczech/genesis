932d3ec6c4bf8c9bbec74e11e85fb50b1dc8f677
Lucas Czech <lucas.czech@h-its.org>
2016-09-13 17:44:47 +0200

    Release v0.10.0

--- lib/utils/core/genesis.hpp
+++ lib/utils/core/genesis.hpp
@@ -59,7 +59,7 @@ namespace genesis {
 inline std::string genesis_version()
 {
     // The following line is automatically replaced by the deploy scripts. Do not change manually.
-    return "v0.9.0"; // #GENESIS_VERSION#
+    return "v0.10.0"; // #GENESIS_VERSION#
 }
 
 /**
