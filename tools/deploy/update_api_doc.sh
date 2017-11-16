deae7fd06d0a7cafff76d1716dfff58aaf90b2dd
Lucas Czech <lucas.czech@h-its.org>
2017-11-16 14:46:43 +0100

    Update doc transfer script.

--- tools/deploy/update_api_doc.sh
+++ tools/deploy/update_api_doc.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Genesis - A toolkit for working with phylogenetic data.
-# Copyright (C) 2014-2016 Lucas Czech
+# Copyright (C) 2014-2017 Lucas Czech
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -121,7 +121,7 @@ if [[ ${do_trans} == 1 ]]; then
     cd html/
 
     remote_host="genesis-lib.org"
-    remote_path="/html/"
+    remote_path="/httpdocs/"
 
     echo -e    "Target:   ${remote_host}${remote_path}doc/"
     read    -p "Username: " remote_user
