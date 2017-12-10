9045d045fe6bf80c538183592a557b84051b55dd
Lucas Czech <lucas.czech@h-its.org>
2017-12-10 22:11:26 +0100

    Release v0.18.1

--- lib/genesis/utils/core/version.hpp
+++ lib/genesis/utils/core/version.hpp
@@ -62,7 +62,7 @@ namespace genesis {
 inline std::string genesis_version()
 {
     // The following line is automatically replaced by the deploy scripts. Do not change manually.
-    return "v0.18.0"; // #GENESIS_VERSION#
+    return "v0.18.1"; // #GENESIS_VERSION#
 }
 
 /**
