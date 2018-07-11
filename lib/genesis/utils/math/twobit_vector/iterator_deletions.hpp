55a1fb3a56969e5b336eea3205d1001bc2588652
Lucas Czech <lucas.czech@h-its.org>
2018-07-11 10:41:13 +0200

    Fix Twobit Vector iterator increment operator return value.

--- lib/genesis/utils/math/twobit_vector/iterator_deletions.hpp
+++ lib/genesis/utils/math/twobit_vector/iterator_deletions.hpp
@@ -3,7 +3,7 @@
 
 /*
     Genesis - A toolkit for working with phylogenetic data.
-    Copyright (C) 2014-2017 Lucas Czech
+    Copyright (C) 2014-2018 Lucas Czech and HITS gGmbH
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -107,7 +107,7 @@ public:
         return &vec_;
     }
 
-    self_type operator ++ ()
+    self_type& operator ++ ()
     {
         // Toy example:
         // Original: ACGT
