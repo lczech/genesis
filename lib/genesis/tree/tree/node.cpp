f458c1137c03f62cb490a02780bbec2129a2751d
Lucas Czech <lucas.czech@h-its.org>
2018-07-12 17:39:24 +0200

    Cleanup Tree Node class.

--- lib/genesis/tree/tree/node.cpp
+++ lib/genesis/tree/tree/node.cpp
@@ -1,6 +1,6 @@
 /*
     Genesis - A toolkit for working with phylogenetic data.
-    Copyright (C) 2014-2017 Lucas Czech
+    Copyright (C) 2014-2018 Lucas Czech and HITS gGmbH
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -42,77 +42,41 @@ namespace tree {
 //     Accessors
 // =================================================================================================
 
-/**
- * @brief Return the index of this Node.
- */
 size_t TreeNode::index() const
 {
     return index_;
 }
 
-/**
- * @brief Return the TreeLink that points towards the root.
- */
 TreeLink& TreeNode::primary_link()
 {
     return *link_;
 }
 
-/**
- * @brief Return the TreeLink that points towards the root.
- */
 TreeLink const& TreeNode::primary_link() const
 {
     return *link_;
 }
 
-/**
- * @brief Return the TreeLink that points towards the root.
- *
- * This is just an alias for primary_link(), that is shorter to use when needed
- * frequently in an algorithm.
- */
 TreeLink& TreeNode::link()
 {
     return *link_;
 }
 
-/**
- * @brief Return the TreeLink that points towards the root.
- *
- * This is just an alias for primary_link(), that is shorter to use when needed
- * frequently in an algorithm.
- */
 TreeLink const& TreeNode::link() const
 {
     return *link_;
 }
 
-/**
- * @brief Return `true` if the TreeNode has a data object assigned to it.
- */
 bool TreeNode::has_data() const
 {
     return data_.get() != nullptr;
 }
 
-/**
- * @brief Return a pointer to the data.
- *
- * In most cases, using data<>() is more convenient. However, in some cases, this function
- * might be necessary.
- */
 BaseNodeData* TreeNode::data_ptr()
 {
     return data_.get();
 }
 
-/**
- * @brief Return a const pointer to the data.
- *
- * In most cases, using data<>() is more convenient. However, in some cases, this function
- * might be necessary.
- */
 BaseNodeData const* TreeNode::data_ptr() const
 {
     return data_.get();
@@ -122,44 +86,18 @@ BaseNodeData const* TreeNode::data_ptr() const
 //     Modifiers
 // =================================================================================================
 
-/**
- * @brief Reset the internal index of this TreeNode.
- *
- * This is a helper function that needs to be used with care and only in cases where appropriate.
- * The index is an invariant that needs to be kept, as it needs to match the index in the Tree
- * container.
- *
- * This function exists to allow building and modifying a Tree without the need for many friend
- * declarations. However, the function should rarely be needed outside of this context.
- */
 TreeNode& TreeNode::reset_index( size_t val )
 {
     index_ = val;
     return *this;
 }
 
-/**
- * @brief Reset the internal pointer to the TreeLink of this TreeNode.
- *
- * This is a helper function that needs to be used with care and only in cases where appropriate.
- *
- * This function exists to allow building and modifying a Tree without the need for many friend
- * declarations. However, the function should rarely be needed outside of this context.
- */
 TreeNode& TreeNode::reset_primary_link( TreeLink* val )
 {
     link_ = val;
     return *this;
 }
 
-/**
- * @brief Reset the data pointer of this TreeNode.
- *
- * Using this function, a TreeNode can be assigend new data. It is also possible to change the
- * data type completely (as long as it derives from BaseNodeData). Be however aware that many
- * functions that work with trees expcet a certain data type. Thus, changing it might break those
- * functions and lead to exceptions and other errors.
- */
 TreeNode& TreeNode::reset_data( std::unique_ptr< BaseNodeData > data )
 {
     data_ = std::move( data );
@@ -170,9 +108,6 @@ TreeNode& TreeNode::reset_data( std::unique_ptr< BaseNodeData > data )
 //     Member Functions
 // =================================================================================================
 
-/**
- * @brief Rank of the node, i.e. how many immediate children it has.
- */
 size_t TreeNode::rank() const
 {
     int rank = -1;
@@ -187,25 +122,16 @@ size_t TreeNode::rank() const
     return static_cast<size_t>(rank);
 }
 
-/**
- * @brief True iff the node is a leaf/tip.
- */
 bool TreeNode::is_leaf() const
 {
     return link_->is_leaf();
 }
 
-/**
- * @brief True iff the node is an inner node.
- */
 bool TreeNode::is_inner() const
 {
     return link_->is_inner();
 }
 
-/**
- * @brief True iff the node is the root of its Tree.
- */
 bool TreeNode::is_root() const
 {
     // The link_ is always the one pointing towards the root. Also, the edge of that link always has
@@ -213,19 +139,6 @@ bool TreeNode::is_root() const
     // At the root itself, however, this means we are pointing to ourselves. Use this to check
     // if this node is the root.
     return &link_->edge().primary_link() == link_;
-}
-
-/**
- * @brief Returns a one-line dump summary of the data of this node.
- *
- * TODO this method assumes that the tree node has a name. not good.
- */
-std::string TreeNode::dump() const
-{
-    // throw std::runtime_error( "Not implemented" );
-    // return "";
-    return "Base node dump";
-    // return "Rank: " + std::to_string(rank()) + " \t " + "name: " + this->data.name;
 }
 
 } // namespace tree
--- lib/genesis/tree/tree/node.hpp
+++ lib/genesis/tree/tree/node.hpp
@@ -3,7 +3,7 @@
 
 /*
     Genesis - A toolkit for working with phylogenetic data.
-    Copyright (C) 2014-2017 Lucas Czech
+    Copyright (C) 2014-2018 Lucas Czech and HITS gGmbH
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -94,14 +94,40 @@ public:
     //     Accessors
     // ---------------------------------------------------------------------
 
+    /**
+     * @brief Return the index of this Node.
+     */
     size_t index() const;
 
+    /**
+     * @brief Return the TreeLink that points towards the root.
+     */
     TreeLink      & primary_link();
+
+    /**
+     * @brief Return the TreeLink that points towards the root.
+     */
     TreeLink const& primary_link() const;
 
+    /**
+     * @brief Return the TreeLink that points towards the root.
+     *
+     * This is just an alias for primary_link(), that is shorter to use when needed
+     * frequently in an algorithm.
+     */
     TreeLink      & link();
+
+    /**
+     * @brief Return the TreeLink that points towards the root.
+     *
+     * This is just an alias for primary_link(), that is shorter to use when needed
+     * frequently in an algorithm.
+     */
     TreeLink const& link() const;
 
+    /**
+     * @brief Return `true` if the TreeNode has a data object assigned to it.
+     */
     bool has_data() const;
 
     template< class NodeDataType >
@@ -128,29 +154,81 @@ public:
         return dynamic_cast< NodeDataType const* >( data_.get() );
     }
 
+    /**
+     * @brief Return a pointer to the data.
+     *
+     * In most cases, using data<>() is more convenient. However, in some cases, this function
+     * might be necessary.
+     */
     BaseNodeData*       data_ptr();
+
+    /**
+     * @brief Return a const pointer to the data.
+     *
+     * In most cases, using data<>() is more convenient. However, in some cases, this function
+     * might be necessary.
+     */
     BaseNodeData const* data_ptr() const;
 
     // ---------------------------------------------------------------------
     //     Modifiers
     // ---------------------------------------------------------------------
 
+    /**
+     * @brief Reset the internal index of this TreeNode.
+     *
+     * This is a helper function that needs to be used with care and only in cases where appropriate.
+     * The index is an invariant that needs to be kept, as it needs to match the index in the Tree
+     * container.
+     *
+     * This function exists to allow building and modifying a Tree without the need for many friend
+     * declarations. However, the function should rarely be needed outside of this context.
+     */
     TreeNode& reset_index( size_t val );
 
+    /**
+     * @brief Reset the internal pointer to the TreeLink of this TreeNode.
+     *
+     * This is a helper function that needs to be used with care and only in cases where appropriate.
+     *
+     * This function exists to allow building and modifying a Tree without the need for many friend
+     * declarations. However, the function should rarely be needed outside of this context.
+     */
     TreeNode& reset_primary_link( TreeLink* val );
 
+    /**
+     * @brief Reset the data pointer of this TreeNode.
+     *
+     * Using this function, a TreeNode can be assigend new data. It is also possible to change the
+     * data type completely (as long as it derives from BaseNodeData). Be however aware that many
+     * functions that work with trees expcet a certain data type. Thus, changing it might break those
+     * functions and lead to exceptions and other errors.
+     */
     TreeNode& reset_data( std::unique_ptr< BaseNodeData > data );
 
     // ---------------------------------------------------------------------
     //     Member Functions
     // ---------------------------------------------------------------------
 
+    /**
+     * @brief Rank of the node, i.e. how many immediate children it has.
+     */
     size_t rank() const;
-    bool   is_leaf() const;
-    bool   is_inner() const;
-    bool   is_root() const;
 
-    std::string dump() const;
+    /**
+     * @brief True iff the node is a leaf/tip.
+     */
+    bool   is_leaf() const;
+
+    /**
+     * @brief True iff the node is an inner node.
+     */
+    bool   is_inner() const;
+
+    /**
+     * @brief True iff the node is the root of its Tree.
+     */
+    bool   is_root() const;
 
     // ---------------------------------------------------------------------
     //     Member Variables
