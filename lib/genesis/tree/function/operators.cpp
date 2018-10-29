32759521bca0593266ddcd582b1213a023e02b45
Lucas Czech <lucas.czech@h-its.org>
2018-10-29 17:45:15 +0100

    Refine tree operators functions.

--- lib/genesis/tree/function/operators.cpp
+++ lib/genesis/tree/function/operators.cpp
@@ -34,6 +34,7 @@
 #include "genesis/tree/iterator/node_links.hpp"
 #include "genesis/tree/iterator/preorder.hpp"
 #include "genesis/tree/printer/compact.hpp"
+#include "genesis/tree/tree/subtree.hpp"
 #include "genesis/utils/core/logging.hpp"
 #include "genesis/utils/core/options.hpp"
 
@@ -46,18 +47,6 @@ namespace tree {
 //     Conversion
 // =================================================================================================
 
-/**
- * @brief Create a tree with the same topology as the source tree, while converting its data.
- *
- * This function takes the given source Tree (possibly with different data types at the nodes and
- * edges), and copies its topology (i.e., all links, nodes and edges, and their structure) to the
- * newly created result tree.
- *
- * The data types are then converted using the two provided functions for the node data type and
- * edge data type, respectively. If a node or an edge does not have data (i.e., the data pointer
- * is a nullptr), the converter functions are not called, but the data of the new tree at that
- * node or edge is also set to a nullptr.
- */
 Tree convert(
     Tree const& source,
     std::function< std::unique_ptr<BaseNodeData>( BaseNodeData const& node_data )> node_data_converter,
@@ -85,22 +74,6 @@ Tree convert(
 //     Equality
 // =================================================================================================
 
-/**
- * @brief Compares two trees for equality given binary comparator functionals for their nodes and
- * edges.
- *
- * This function does a preorder traversal of both trees in parallel and calls the comparator
- * functionals for each position of the iterator. It returns true iff the comparators are true for
- * every position.
- *
- * The comparator functionals can be either function pointers, function objects, or lambda
- * expressions.
- *
- * As the traversal is done in parallel, the trees are also checked for equal topology:
- * their elements (links, nodes, edges) have to be equal in size and the degree of each node during
- * the traversal has to be identical in both trees. Those assumptions are made because two trees
- * that do not have identical topology are never considered equal.
- */
 bool equal(
     Tree const& lhs,
     Tree const& rhs,
@@ -165,14 +138,6 @@ bool equal(
 //     return equal<TreeTypeL, TreeTypeR>(lhs, rhs, node_comparator, edge_comparator);
 // }
 
-/**
- * @brief Returns true iff both trees have an identical topology.
- *
- * The topology is considered identical only if the order of edges is also the same in both trees.
- * This means, although two trees might have the same number of leaves and branches, they might
- * still be not identical (with respect to this function) when the branches appear in a different
- * order or when the root sits at a different node.
- */
 bool identical_topology( Tree const& lhs, Tree const& rhs)
 {
     auto node_comparator = [] (
@@ -196,57 +161,46 @@ bool identical_topology( Tree const& lhs, Tree const& rhs)
     return equal( lhs, rhs, node_comparator, edge_comparator );
 }
 
-/**
- * @brief Return whether the TreeNode belongs to the Tree, i.e., whether it is owned by the Tree.
- */
 bool belongs_to( Tree const& tree, TreeNode const& node )
 {
     return node.index() < tree.node_count() && &tree.node_at( node.index() ) == &node;
 }
 
-/**
- * @brief Return whether the TreeNode belongs to the Tree, i.e., whether it is owned by the Tree.
- */
 bool belongs_to( TreeNode const& node, Tree const& tree )
 {
     return node.index() < tree.node_count() && &tree.node_at( node.index() ) == &node;
 }
 
-/**
- * @brief Return whether the TreeEdge belongs to the Tree, i.e., whether it is owned by the Tree.
- */
 bool belongs_to( Tree const& tree, TreeEdge const& edge )
 {
     return edge.index() < tree.edge_count() && &tree.edge_at( edge.index() ) == &edge;
 }
 
-/**
- * @brief Return whether the TreeEdge belongs to the Tree, i.e., whether it is owned by the Tree.
- */
 bool belongs_to( TreeEdge const& edge, Tree const& tree )
 {
     return edge.index() < tree.edge_count() && &tree.edge_at( edge.index() ) == &edge;
 }
 
-/**
- * @brief Return whether the TreeLink belongs to the Tree, i.e., whether it is owned by the Tree.
- */
 bool belongs_to( Tree const& tree, TreeLink const& link )
 {
     return link.index() < tree.link_count() && &tree.link_at( link.index() ) == &link;
 }
 
-/**
- * @brief Return whether the TreeLink belongs to the Tree, i.e., whether it is owned by the Tree.
- */
 bool belongs_to( TreeLink const& link, Tree const& tree )
 {
     return link.index() < tree.link_count() && &tree.link_at( link.index() ) == &link;
 }
 
-/**
- * @brief Return the TreeEdge between two TreeNode&s, if they are neighbours, or `nullptr` otherwise.
- */
+bool belongs_to( Tree     const& tree, Subtree  const& subt )
+{
+    return belongs_to( subt.link(), tree );
+}
+
+bool belongs_to( Subtree  const& subt, Tree     const& tree )
+{
+    return belongs_to( subt.link(), tree );
+}
+
 TreeEdge* edge_between( TreeNode& lhs, TreeNode& rhs )
 {
     // No need to check whether the two nodes belong to the same tree.
@@ -259,9 +213,6 @@ TreeEdge* edge_between( TreeNode& lhs, TreeNode& rhs )
     return nullptr;
 }
 
-/**
- * @brief Return the TreeEdge between two TreeNode&s, if they are neighbours, or `nullptr` otherwise.
- */
 TreeEdge const* edge_between( TreeNode const& lhs, TreeNode const& rhs )
 {
     // No need to check whether the two nodes belong to the same tree.
@@ -355,12 +306,6 @@ std::ostream& operator << ( std::ostream& out, TreeNode const& node )
 //     Validate
 // =================================================================================================
 
-/**
- * @brief Validate that all internal pointers of the Tree elements (TreeLink%s, TreeNode%s,
- * TreeEdge%s) to each other are correct and that some other invariants are met.
- *
- * This check is a bit pedantic, but better safe than sorry.
- */
 bool validate_topology( Tree const& tree )
 {
     // -----------------------------------------------------
--- lib/genesis/tree/function/operators.hpp
+++ lib/genesis/tree/function/operators.hpp
@@ -47,6 +47,7 @@ namespace tree {
 
 class BaseNodeData;
 class BaseEdgeData;
+class Subtree;
 
 // =================================================================================================
 //     Data Type Checks
@@ -118,6 +119,18 @@ bool tree_data_is_derived_from( Tree const& tree )
 //     Conversion
 // =================================================================================================
 
+/**
+ * @brief Create a tree with the same topology as the source tree, while converting its data.
+ *
+ * This function takes the given source Tree (possibly with different data types at the nodes and
+ * edges), and copies its topology (i.e., all links, nodes and edges, and their structure) to the
+ * newly created result tree.
+ *
+ * The data types are then converted using the two provided functions for the node data type and
+ * edge data type, respectively. If a node or an edge does not have data (i.e., the data pointer
+ * is a nullptr), the converter functions are not called, but the data of the new tree at that
+ * node or edge is also set to a nullptr.
+ */
 Tree convert(
     Tree const& source,
     std::function< std::unique_ptr<BaseNodeData>( BaseNodeData const& node_data )> node_data_converter,
@@ -128,6 +141,22 @@ Tree convert(
 //     Equality
 // =================================================================================================
 
+/**
+ * @brief Compares two trees for equality given binary comparator functionals for their nodes and
+ * edges.
+ *
+ * This function does a preorder traversal of both trees in parallel and calls the comparator
+ * functionals for each position of the iterator. It returns true iff the comparators are true for
+ * every position.
+ *
+ * The comparator functionals can be either function pointers, function objects, or lambda
+ * expressions.
+ *
+ * As the traversal is done in parallel, the trees are also checked for equal topology:
+ * their elements (links, nodes, edges) have to be equal in size and the degree of each node during
+ * the traversal has to be identical in both trees. Those assumptions are made because two trees
+ * that do not have identical topology are never considered equal.
+ */
 bool equal(
     Tree const& lhs,
     Tree const& rhs,
@@ -137,16 +166,64 @@ bool equal(
 
 // bool equal( Tree const& lhs, Tree const& rhs);
 
+/**
+ * @brief Returns true iff both trees have an identical topology.
+ *
+ * The topology is considered identical only if the order of edges is also the same in both trees.
+ * This means, although two trees might have the same number of leaves and branches, they might
+ * still be not identical (with respect to this function) when the branches appear in a different
+ * order or when the root sits at a different node.
+ */
 bool identical_topology( Tree const& lhs, Tree const& rhs);
 
+/**
+ * @brief Return whether the TreeNode belongs to the Tree, i.e., whether it is owned by the Tree.
+ */
 bool belongs_to( Tree     const& tree, TreeNode const& node );
+
+/**
+ * @brief Return whether the TreeNode belongs to the Tree, i.e., whether it is owned by the Tree.
+ */
 bool belongs_to( TreeNode const& node, Tree     const& tree );
+
+/**
+ * @brief Return whether the TreeEdge belongs to the Tree, i.e., whether it is owned by the Tree.
+ */
 bool belongs_to( Tree     const& tree, TreeEdge const& edge );
+
+/**
+ * @brief Return whether the TreeEdge belongs to the Tree, i.e., whether it is owned by the Tree.
+ */
 bool belongs_to( TreeEdge const& edge, Tree     const& tree );
+
+/**
+ * @brief Return whether the TreeLink belongs to the Tree, i.e., whether it is owned by the Tree.
+ */
 bool belongs_to( Tree     const& tree, TreeLink const& link );
+
+/**
+ * @brief Return whether the TreeLink belongs to the Tree, i.e., whether it is owned by the Tree.
+ */
 bool belongs_to( TreeLink const& link, Tree     const& tree );
 
+/**
+ * @brief Return whether the Subtree belongs to the Tree, i.e., whether its link is owned by the Tree.
+ */
+bool belongs_to( Tree     const& tree, Subtree  const& subt );
+
+/**
+ * @brief Return whether the Subtree belongs to the Tree, i.e., whether its link is owned by the Tree.
+ */
+bool belongs_to( Subtree  const& subt, Tree     const& tree );
+
+/**
+ * @brief Return the TreeEdge between two TreeNode&s, if they are neighbours, or `nullptr` otherwise.
+ */
 TreeEdge*       edge_between( TreeNode& lhs,       TreeNode& rhs );
+
+/**
+ * @brief Return the TreeEdge between two TreeNode&s, if they are neighbours, or `nullptr` otherwise.
+ */
 TreeEdge const* edge_between( TreeNode const& lhs, TreeNode const& rhs );
 
 // =================================================================================================
@@ -169,6 +246,12 @@ std::ostream& operator << ( std::ostream& out, TreeNode const& node );
 //     Validate
 // =================================================================================================
 
+/**
+ * @brief Validate that all internal pointers of the Tree elements (TreeLink%s, TreeNode%s,
+ * TreeEdge%s) to each other are correct and that some other invariants are met.
+ *
+ * This check is a bit pedantic, but better safe than sorry.
+ */
 bool validate_topology( Tree const& tree );
 
 } // namespace tree
