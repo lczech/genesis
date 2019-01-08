73de7a79a1deeb0718c008a7872bc3c966babaa9
Lucas Czech <lucas.czech@h-its.org>
2019-01-08 14:59:42 +0100

    Fix weird issue on Mac OS with template class default construction of strings from chars.

--- lib/genesis/utils/containers/matrix/writer.hpp
+++ lib/genesis/utils/containers/matrix/writer.hpp
@@ -3,7 +3,7 @@
 
 /*
     Genesis - A toolkit for working with phylogenetic data.
-    Copyright (C) 2014-2018 Lucas Czech and HITS gGmbH
+    Copyright (C) 2014-2019 Lucas Czech and HITS gGmbH
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -316,7 +316,7 @@ private:
 
 private:
 
-    std::string separator_ = "\t";
+    std::string separator_ = std::string( "\t" );
     Format      format_ = Format::kMatrix;
 
     std::function<std::string( T const& )> write_value_;
