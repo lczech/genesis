b63a44e6bc9b4bf57dca1e72da81a5d47b581669
Lucas Czech <lucas.czech@h-its.org>
2019-04-23 14:21:51 +0200

    Refine Tree distance calculations.

--- test/src/tree/common_tree/distances.cpp
+++ test/src/tree/common_tree/distances.cpp
@@ -1,6 +1,6 @@
 /*
     Genesis - A toolkit for working with phylogenetic data.
-    Copyright (C) 2014-2018 Lucas Czech and HITS gGmbH
+    Copyright (C) 2014-2019 Lucas Czech and HITS gGmbH
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -39,6 +39,7 @@
 #include "genesis/tree/formats/newick/reader.hpp"
 #include "genesis/tree/tree.hpp"
 #include "genesis/utils/containers/matrix.hpp"
+#include "genesis/utils/containers/matrix/operators.hpp"
 
 using namespace genesis;
 using namespace tree;
@@ -76,4 +77,35 @@ TEST(CommonTree, Distances)
 
     EXPECT_TRUE( exp == edge_branch_length_distance_matrix(tree) );
     EXPECT_EQ(   exp,   edge_branch_length_distance_matrix(tree) );
+}
+
+TEST( CommonTree, PatristicDistances )
+{
+    // Skip test if no data availabe.
+    NEEDS_TEST_DATA;
+
+    // Read and process tree.
+    std::string const infile = environment->data_dir + "tree/distances.newick";
+    Tree const tree =  CommonTreeNewickReader().read( utils::from_file( infile ));
+
+    utils::Matrix<double> exp (13, 13, {
+        0, 2048, 3072, 2560, 2816, 2944, 2880, 2592, 16, 24, 28, 26, 17,
+        2048, 0, 1024, 512, 768, 896, 832, 544, 2064, 2072, 2076, 2074, 2065,
+        3072, 1024, 0, 1536, 1792, 1920, 1856, 1568, 3088, 3096, 3100, 3098, 3089,
+        2560, 512, 1536, 0, 256, 384, 320, 32, 2576, 2584, 2588, 2586, 2577,
+        2816, 768, 1792, 256, 0, 128, 64, 288, 2832, 2840, 2844, 2842, 2833,
+        2944, 896, 1920, 384, 128, 0, 192, 416, 2960, 2968, 2972, 2970, 2961,
+        2880, 832, 1856, 320, 64, 192, 0, 352, 2896, 2904, 2908, 2906, 2897,
+        2592, 544, 1568, 32, 288, 416, 352, 0, 2608, 2616, 2620, 2618, 2609,
+        16, 2064, 3088, 2576, 2832, 2960, 2896, 2608, 0, 8, 12, 10, 1,
+        24, 2072, 3096, 2584, 2840, 2968, 2904, 2616, 8, 0, 4, 2, 9,
+        28, 2076, 3100, 2588, 2844, 2972, 2908, 2620, 12, 4, 0, 6, 13,
+        26, 2074, 3098, 2586, 2842, 2970, 2906, 2618, 10, 2, 6, 0, 11,
+        17, 2065, 3089, 2577, 2833, 2961, 2897, 2609, 1, 9, 13, 11, 0
+    });
+
+    auto const mat = node_branch_length_distance_matrix( tree );
+    // LOG_DBG << mat;
+
+    EXPECT_EQ( exp, mat );
 }
