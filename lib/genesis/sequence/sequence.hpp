883b0ae768721da40319af2160859202ea177793
Lucas Czech <lucas.czech@h-its.org>
2020-03-22 00:25:53 -0700

    Minor enhancement of Sequence

--- lib/genesis/sequence/sequence.hpp
+++ lib/genesis/sequence/sequence.hpp
@@ -3,7 +3,7 @@
 
 /*
     Genesis - A toolkit for working with phylogenetic data.
-    Copyright (C) 2014-2019 Lucas Czech and HITS gGmbH
+    Copyright (C) 2014-2020 Lucas Czech and HITS gGmbH
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -88,6 +88,11 @@ public:
         label_ = value;
     }
 
+    void label( std::string&& value )
+    {
+        label_ = std::move( value );
+    }
+
     std::string const& sites() const
     {
         return sites_;
@@ -100,7 +105,7 @@ public:
 
     void sites( std::string && value )
     {
-        sites_ = std::move(value);
+        sites_ = std::move( value );
     }
 
     std::string& sites()
