5093e0283acb262d0cb5fccefc605771dd6ecd64
Lucas Czech <lucas.czech@h-its.org>
2020-04-05 20:51:27 -0700

    Add date/time parsing functions to std::tm

--- lib/genesis/utils/tools/date_time.cpp
+++ lib/genesis/utils/tools/date_time.cpp
@@ -1,6 +1,6 @@
 /*
     Genesis - A toolkit for working with phylogenetic data.
-    Copyright (C) 2014-2019 Lucas Czech and HITS gGmbH
+    Copyright (C) 2014-2020 Lucas Czech and HITS gGmbH
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -30,19 +30,35 @@
 
 #include "genesis/utils/tools/date_time.hpp"
 
+#include "genesis/utils/text/string.hpp"
+
+#include "genesis/utils/core/logging.hpp"
+
+#include <array>
 #include <cstdio>
 #include <ctime>
+#include <iomanip>
+#include <sstream>
 
 namespace genesis {
 namespace utils {
 
+// =================================================================================================
+//     Convenience Functions
+// =================================================================================================
+
 std::string current_date()
 {
-    time_t now = std::time( nullptr );
-    tm*    ltm = std::localtime( &now );
+    std::time_t now = std::time( nullptr );
+    std::tm*    ltm = std::localtime( &now );
+
+    if( !ltm ) {
+        throw std::runtime_error( "Cannot get current date." );
+    }
 
     char out[12];
-    std::sprintf (out, "%u-%02u-%02u",
+    std::sprintf(
+        out, "%u-%02u-%02u",
         ltm->tm_year + 1900, ltm->tm_mon + 1, ltm->tm_mday
     );
     return out;
@@ -50,12 +66,125 @@ std::string current_date()
 
 std::string current_time()
 {
-    time_t now = std::time( nullptr );
-    tm*    ltm = std::localtime( &now );
+    std::time_t now = std::time( nullptr );
+    std::tm*    ltm = std::localtime( &now );
+
+    if( !ltm ) {
+        throw std::runtime_error( "Cannot get current time." );
+    }
 
     char out[10];
-    std::sprintf (out, "%02u:%02u:%02u", ltm->tm_hour, ltm->tm_min, ltm->tm_sec);
+    std::sprintf(
+        out, "%02u:%02u:%02u",
+        ltm->tm_hour, ltm->tm_min, ltm->tm_sec
+    );
     return out;
+}
+
+// =================================================================================================
+//     Date/Time Conversion to std::tm
+// =================================================================================================
+
+// Typical locales that we expect to see in scientific data.
+static const std::array<std::string, 3> locales_ = { "C", "en_US.UTF-8", "" };
+
+// Typical formats that we expect to see in scientific data.
+static const std::array<std::string, 9> formats_ = {
+    "%Y-%m-%d", "%Y%m%d", "%Y-%m-%dT%H:%M:%S", "%Y-%m-%d %H:%M:%S", "%Y%m%dT%H%M%S",
+    "%Y%m%d %H%M%S", "%Y%m%d%H%M%S", "%H:%M:%S", "%H%M%S"
+};
+
+/**
+ * @brief Local helper function that does the heavy load of time conversion.
+ *
+ * We use this variant, which returns its success, to not have to `try catch` in the functions
+ * that guess the format.
+ */
+bool convert_to_tm_( std::string const& str, std::string const& format, std::string const& locale, std::tm& t )
+{
+    // Init the tm object to all zeros, see https://en.cppreference.com/w/cpp/io/manip/get_time
+    t.tm_sec = t.tm_min = t.tm_hour = t.tm_mday = t.tm_mon = t.tm_year = t.tm_wday = t.tm_yday = t.tm_isdst = 0;
+
+    // Run the conversion, using all provided information.
+    std::istringstream ss( trim( str ));
+    ss.imbue( std::locale( locale ));
+    ss >> std::get_time( &t, format.c_str() );
+
+    // Return whether that worked or failed, and whether we also reached the end of the stream.
+    // If we did not reach EOF, there is more data in the stream, which means, we only partically
+    // matched the string, so that it is not an actual fit.
+    return ! ss.fail() && ss.eof();
+}
+
+std::tm convert_to_tm( std::string const& str, std::string const& format, std::string const& locale )
+{
+    std::tm t = {};
+    if( !convert_to_tm_( str, format, locale, t )) {
+        throw std::invalid_argument(
+            "Cannot convert string to tm date/time object."
+        );
+    }
+    return t;
+}
+
+std::tm convert_to_tm( std::string const& str, std::string const& format )
+{
+    std::tm t = {};
+    for( auto const& locale : locales_ ) {
+        if( convert_to_tm_( str, format, locale, t )) {
+            return t;
+        }
+    }
+
+    throw std::invalid_argument(
+        "Cannot convert string to tm date/time object with given format."
+    );
+}
+
+std::tm convert_to_tm( std::string const& str )
+{
+    std::tm t = {};
+
+    // The formats that we try here are not dependent on the locale. If we introduce additional
+    // formats in the future, it might be necessary to also loop over those, for example the
+    // user-preferred locale, to make sure that local date/time formats can be parsed.
+    for( auto const& format : formats_ ) {
+        if( convert_to_tm_( str, format, "C", t )) {
+            return t;
+        }
+    }
+
+    throw std::invalid_argument(
+        "Cannot convert string to tm date/time object with guessed formats."
+    );
+}
+
+bool is_convertible_to_tm( std::string const& str, std::string const& format, std::string const& locale )
+{
+    std::tm t = {};
+    return convert_to_tm_( str, format, locale, t );
+}
+
+bool is_convertible_to_tm( std::string const& str, std::string const& format )
+{
+    std::tm t = {};
+    for( auto const& locale : locales_ ) {
+        if( convert_to_tm_( str, format, locale, t )) {
+            return true;
+        }
+    }
+    return false;
+}
+
+bool is_convertible_to_tm( std::string const& str )
+{
+    std::tm t = {};
+    for( auto const& format : formats_ ) {
+        if( convert_to_tm_( str, format, "C", t )) {
+            return true;
+        }
+    }
+    return false;
 }
 
 } // namespace utils
---
+++ test/src/utils/tools/date_time.cpp
@@ -0,0 +1,65 @@
+/*
+    Genesis - A toolkit for working with phylogenetic data.
+    Copyright (C) 2014-2020 Lucas Czech
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+    Contact:
+    Lucas Czech <lucas.czech@h-its.org>
+    Exelixis Lab, Heidelberg Institute for Theoretical Studies
+    Schloss-Wolfsbrunnenweg 35, D-69118 Heidelberg, Germany
+*/
+
+/**
+ * @brief
+ *
+ * @file
+ * @ingroup test
+ */
+
+#include "src/common.hpp"
+
+#include "genesis/utils/tools/date_time.hpp"
+
+#include <string>
+#include <vector>
+
+using namespace genesis::utils;
+
+TEST( DateTime, ConversionTM )
+{
+    std::vector<std::string> valids = {
+        "2020-04-17 ", " 20200417", " 2020-04-17T00:27:58 ", "2020-04-17 00:27:58\t", "\t20200417T002758",
+        "\n20200417 002758 \t", "    20200417002758", "\n\n\t00:27:58", "002758\t\t\n"
+    };
+
+    for( auto const& valid : valids ) {
+        EXPECT_NO_THROW( convert_to_tm( valid ));
+        EXPECT_TRUE( is_convertible_to_tm( valid ));
+    }
+    EXPECT_NO_THROW( convert_to_tm( valids.begin(), valids.end() ));
+    EXPECT_TRUE( is_convertible_to_tm( valids.begin(), valids.end() ));
+
+    std::vector<std::string> invalids = {
+        "2020/04/17", "04/17/2020", "2020-04-17T00:27:58Z", "2020-04-17 00:27:58Z", "20200417T002758+0100",
+        "20200417x002758", "120200417002758", "00:27:58+0", "02758", "What time is it?"
+    };
+
+    for( auto const& valid : invalids ) {
+        EXPECT_ANY_THROW( convert_to_tm( valid ));
+        EXPECT_FALSE( is_convertible_to_tm( valid ));
+    }
+    EXPECT_ANY_THROW( convert_to_tm( invalids.begin(), invalids.end() ));
+    EXPECT_FALSE( is_convertible_to_tm( invalids.begin(), invalids.end() ));
+}
