8df13f9c93c527b4d86f42dd067df5f94c3d7560
Lucas Czech <lucas.czech@h-its.org>
2020-04-12 20:59:26 -0700

    Fix clang error on trusty

--- lib/genesis/utils/formats/svg/matrix.hpp
+++ lib/genesis/utils/formats/svg/matrix.hpp
@@ -3,7 +3,7 @@
 
 /*
     Genesis - A toolkit for working with phylogenetic data.
-    Copyright (C) 2014-2019 Lucas Czech and HITS gGmbH
+    Copyright (C) 2014-2020 Lucas Czech and HITS gGmbH
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -81,9 +81,20 @@ public:
 
 SvgGroup make_svg_matrix(
     Matrix<Color> const& mat,
-    SvgMatrixSettings settings = {},
-    std::vector<std::string> const& row_labels = {},
-    std::vector<std::string> const& col_labels = {}
+    SvgMatrixSettings settings,
+    std::vector<std::string> const& row_labels,
+    std::vector<std::string> const& col_labels
+);
+
+SvgGroup make_svg_matrix(
+    Matrix<Color> const& mat,
+    SvgMatrixSettings settings,
+    std::vector<std::string> const& row_labels
+);
+
+SvgGroup make_svg_matrix(
+    Matrix<Color> const& mat,
+    SvgMatrixSettings settings = {}
 );
 
 } // namespace utils
