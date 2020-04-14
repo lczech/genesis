5854403e4bc7e5f9a8119817cdd666990bbbc8fe
Lucas Czech <lucas.czech@h-its.org>
2020-04-14 16:21:11 -0700

    Fix bugs found by gcc 10

--- lib/genesis/tree/function/functions.cpp
+++ lib/genesis/tree/function/functions.cpp
@@ -1,6 +1,6 @@
 /*
     Genesis - A toolkit for working with phylogenetic data.
-    Copyright (C) 2014-2019 Lucas Czech and HITS gGmbH
+    Copyright (C) 2014-2020 Lucas Czech and HITS gGmbH
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -427,7 +427,7 @@ std::vector<size_t> subtree_sizes( Tree const& tree, TreeNode const& node )
 
     // The size of the subtree of the starting node is always the number of nodes in the tree
     // minus one for that node itself (as it is not counted as part of its subtree).
-    assert( result[ node.index() ] = tree.node_count() - 1 );
+    assert( result[ node.index() ] == tree.node_count() - 1 );
 
     return result;
 }
