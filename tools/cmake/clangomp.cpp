3ac12a6de9aa1ddccc0addb602c4f7aa506c59c5
Lucas Czech <lucas.czech@h-its.org>
2020-04-15 00:41:01 -0700

    More bionic clang openmp tweaks

--- tools/cmake/clangomp.cpp
+++ tools/cmake/clangomp.cpp
@@ -2,6 +2,13 @@
 #include <stdio.h>
 
 int main() {
+
+    #ifdef _OPENMP
+        printf("Hello from _OPENMP\n");
+    #else
+      breaks_on_purpose
+    #endif
+
     #pragma omp parallel
     printf("Hello from thread %d, nthreads %d\n", omp_get_thread_num(), omp_get_num_threads());
     return 0;
