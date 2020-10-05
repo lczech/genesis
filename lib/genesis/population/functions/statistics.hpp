4a5012ac801805e5c5d26806a63898ad33e5d3b7
Lucas Czech <lucas.czech@h-its.org>
2020-10-05 15:59:01 -0700

    Add theta pi documentation

--- lib/genesis/population/functions/statistics.hpp
+++ lib/genesis/population/functions/statistics.hpp
@@ -48,14 +48,48 @@ namespace population {
 //     Diversity Estimates
 // =================================================================================================
 
+/**
+ * @brief Compute classic heterozygosity.
+ *
+ * This is computed as \f$ h = \frac{n}{n-1} \left( 1 - \sum p^2 \right) \f$ with `n` the total
+ * nucleotide_sum() (sum of `A`,`C`,`G`,`T` in the sample), and `p` their respective nucleotide
+ * frequencies.
+ *
+ * See Equation 3.1 in
+ *
+ * > Hahn, M. W. (2018). Molecular Population Genetics.
+ * > https://global.oup.com/academic/product/molecular-population-genetics-9780878939657?cc=us&lang=en&
+ *
+ * for details.
+ */
 double heterozygosity( PoolSample const& sample );
 
 // =================================================================================================
 //     Theta Pi
 // =================================================================================================
 
+/**
+ * @brief Compute the denominator for the pool-sequencing correction of theta pi according to
+ * Kofler et al.
+ *
+ * See
+ *
+ * > R. Kofler, P. Orozco-terWengel, N. De Maio, R. V. Pandey, V. Nolte,
+ * > A. Futschik, C. Kosiol, C.Schlötterer.</br>
+ * > PoPoolation: A Toolbox for Population Genetic Analysis of
+ * > Next Generation Sequencing Data from Pooled Individuals.</br>
+ * > 2011. PLoS ONE, 6(1), e15925. https://doi.org/10.1371/journal.pone.0015925
+ *
+ * for details. We here compute the denominator for a given @p poolsize, with a fix @p min_allele_count,
+ * which is identical for each given @p nucleotide_count, and henced cached internally for speedup.
+ */
 double theta_pi_pool_denominator( size_t poolsize, size_t min_allele_count, size_t nucleotide_count );
 
+/**
+ * @brief Compute classic theta pi, that is, the sum of heterozygosities.
+ *
+ * The function sums heterozygosity() for all samples in the given range.
+ */
 template<class ForwardIterator>
 double theta_pi(
     ForwardIterator begin, ForwardIterator end
@@ -67,6 +101,23 @@ double theta_pi(
     return pi_sum;
 }
 
+/**
+ * @brief Compute theta pi with pool-sequencing correction according to Kofler et al,
+ * that is, the sum of heterozygosities divided by the correction denominator.
+ *
+ * The function sums heterozygosity() for all samples in the given range, and divides each
+ * by the respective denominator to correct for error from pool sequencing.
+ *
+ * See
+ *
+ * > R. Kofler, P. Orozco-terWengel, N. De Maio, R. V. Pandey, V. Nolte, A.
+ * > Futschik, C. Kosiol, C.Schlötterer.</br>
+ * > PoPoolation: A Toolbox for Population Genetic Analysis of
+ * > Next Generation Sequencing Data from Pooled Individuals.</br>
+ * > 2011. PLoS ONE, 6(1), e15925. https://doi.org/10.1371/journal.pone.0015925
+ *
+ * for details.
+ */
 template<class ForwardIterator>
 double theta_pi_pool( // get_pi_calculator
     ForwardIterator begin,
