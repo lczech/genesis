906883da95e79106cd2f030f6cbdff4441154695
Lucas Czech <lucas.czech@h-its.org>
2020-10-08 17:07:49 -0700

    Fix copyright notice in test files

--- test/src/population/pileup.cpp
+++ test/src/population/pileup.cpp
@@ -1,6 +1,6 @@
 /*
     Genesis - A toolkit for working with phylogenetic data.
-    Copyright (C) 2014-2020
+    Copyright (C) 2014-2020 Lucas Czech
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
