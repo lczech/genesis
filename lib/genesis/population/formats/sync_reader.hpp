76fff5bea16c656979a09cc7282f8dac619713d0
Lucas Czech <lucas.czech@h-its.org>
2021-04-07 21:42:32 -0700

    Fix typo in Sync Reader doc

--- lib/genesis/population/formats/sync_reader.hpp
+++ lib/genesis/population/formats/sync_reader.hpp
@@ -3,7 +3,7 @@
 
 /*
     Genesis - A toolkit for working with phylogenetic data.
-    Copyright (C) 2014-2020 Lucas Czech
+    Copyright (C) 2014-2021 Lucas Czech
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -19,9 +19,9 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
     Contact:
-    Lucas Czech <lucas.czech@h-its.org>
-    Exelixis Lab, Heidelberg Institute for Theoretical Studies
-    Schloss-Wolfsbrunnenweg 35, D-69118 Heidelberg, Germany
+    Lucas Czech <lczech@carnegiescience.edu>
+    Department of Plant Biology, Carnegie Institution For Science
+    260 Panama Street, Stanford, CA 94305, USA
 */
 
 /**
@@ -48,7 +48,7 @@ namespace population {
 /**
  * @brief Reader for PoPoolation's "synchronized" files.
  *
- * These files are a simple tally of the coutns at each position and sample in a (m)pileup file.
+ * These files are a simple tally of the counts at each position and sample in a (m)pileup file.
  * See https://sourceforge.net/p/popoolation2/wiki/Tutorial/ for the format description.
  */
 class SyncReader
