1989c3860436296a056f50dbfd9d2cae0f5b5303
Lucas Czech <lucas.czech@h-its.org>
2021-04-21 16:37:23 -0700

    Add Genome Region helper functions

--- lib/genesis/population/functions/genome_region.cpp
+++ lib/genesis/population/functions/genome_region.cpp
@@ -1,6 +1,6 @@
 /*
     Genesis - A toolkit for working with phylogenetic data.
-    Copyright (C) 2014-2020 Lucas Czech
+    Copyright (C) 2014-2021 Lucas Czech
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -16,9 +16,9 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
     Contact:
-    Lucas Czech <lucas.czech@h-its.org>
-    Exelixis Lab, Heidelberg Institute for Theoretical Studies
-    Schloss-Wolfsbrunnenweg 35, D-69118 Heidelberg, Germany
+    Lucas Czech <lczech@carnegiescience.edu>
+    Department of Plant Biology, Carnegie Institution For Science
+    260 Panama Street, Stanford, CA 94305, USA
 */
 
 /**
@@ -126,6 +126,18 @@ GenomeRegion parse_genome_region( std::string const& region )
         // Multiple ":" found.
         throw_invalid_arg_();
     }
+    return result;
+}
+
+GenomeRegionList parse_genome_regions( std::string const& regions )
+{
+    GenomeRegionList result;
+
+    auto const region_list = utils::split( regions, ",", false );
+    for( auto const& region : region_list ) {
+        result.add( parse_genome_region( utils::trim( region )));
+    }
+
     return result;
 }
 
--- lib/genesis/population/functions/genome_region.hpp
+++ lib/genesis/population/functions/genome_region.hpp
@@ -3,7 +3,7 @@
 
 /*
     Genesis - A toolkit for working with phylogenetic data.
-    Copyright (C) 2014-2020 Lucas Czech
+    Copyright (C) 2014-2021 Lucas Czech
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -19,9 +19,9 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
     Contact:
-    Lucas Czech <lucas.czech@h-its.org>
-    Exelixis Lab, Heidelberg Institute for Theoretical Studies
-    Schloss-Wolfsbrunnenweg 35, D-69118 Heidelberg, Germany
+    Lucas Czech <lczech@carnegiescience.edu>
+    Department of Plant Biology, Carnegie Institution For Science
+    260 Panama Street, Stanford, CA 94305, USA
 */
 
 /**
@@ -33,7 +33,9 @@
 
 #include "genesis/population/genome_region.hpp"
 
+#include <cassert>
 #include <iosfwd>
+#include <stdexcept>
 #include <string>
 #include <vector>
 
@@ -49,7 +51,47 @@ std::string to_string( GenomeRegion const& region );
 
 bool operator ==( GenomeRegion const& a, GenomeRegion const& b );
 
+/**
+ * @brief Parse a genomic region.
+ *
+ * Accepted formats are "chromosome", "chromosome:position", "chromosome:start-end", and
+ * "chromosome:start..end".
+ */
 GenomeRegion parse_genome_region( std::string const& region );
+
+/**
+ * @brief Parse a set/list of genomic regions.
+ *
+ * The individual regions need to be separated by commas (surrounding white space is okay),
+ * and each region needs to follow the format as explained in parse_genome_region().
+ */
+GenomeRegionList parse_genome_regions( std::string const& regions );
+
+/**
+ * @brief Test whether the chromosome/position of a @p variant is within a given genomic @p region.
+ *
+ * This is a function template, so that it can accept any data structure that contains public
+ * member variables `chromosome` (`std::string`) and `position` (`size_t`), such as Variant.
+ */
+template<class T>
+bool is_covered( GenomeRegion const& region, T const& variant )
+{
+    if( region.start > region.end ) {
+        throw std::runtime_error( "Invalid GenomeRegion with start > end" );
+    }
+
+    if( region.start > 0 || region.end > 0 ) {
+        // With proper start and/or end, all has to match.
+        auto const chr = variant.chromosome == region.chromosome;
+        auto const beg = variant.position >= region.start;
+        auto const end = variant.position <= region.end;
+        return chr && beg && end;
+    } else {
+        // If both start and end are zero, we are just matching the chromosome.
+        assert( region.start == 0 && region.end == 0 );
+        return variant.chromosome == region.chromosome;
+    }
+}
 
 } // namespace population
 } // namespace genesis
