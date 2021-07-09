237f30406eac02efaec9897093ccf6b020993cbb
Lucas Czech <lucas.czech@h-its.org>
2021-07-09 14:33:18 -0700

    Add Histogram default constructor

--- lib/genesis/utils/math/histogram.hpp
+++ lib/genesis/utils/math/histogram.hpp
@@ -3,7 +3,7 @@
 
 /*
     Genesis - A toolkit for working with phylogenetic data.
-    Copyright (C) 2014-2019 Lucas Czech and HITS gGmbH
+    Copyright (C) 2014-2021 Lucas Czech and HITS gGmbH
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -19,9 +19,9 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
     Contact:
-    Lucas Czech <lucas.czech@h-its.org>
-    Exelixis Lab, Heidelberg Institute for Theoretical Studies
-    Schloss-Wolfsbrunnenweg 35, D-69118 Heidelberg, Germany
+    Lucas Czech <lczech@carnegiescience.edu>
+    Department of Plant Biology, Carnegie Institution For Science
+    260 Panama Street, Stanford, CA 94305, USA
 */
 
 /**
@@ -85,6 +85,8 @@ public:
     // -------------------------------------------------------------------------
     //     Constructors and Rule of Five
     // -------------------------------------------------------------------------
+
+    Histogram() = default;
 
     explicit Histogram(
         size_t num_bins
@@ -201,7 +203,7 @@ private:
     std::vector<double> bins_;
     std::vector<double> ranges_;
 
-    OutOfRangeBehaviour out_of_range_behaviour_;
+    OutOfRangeBehaviour out_of_range_behaviour_ = OutOfRangeBehaviour::kThrow;
 };
 
 } // namespace utils
