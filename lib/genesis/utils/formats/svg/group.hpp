b4fc298ad896e81ad4907c3bb24a6eaa1c5511d7
Lucas Czech <lucas.czech@h-its.org>
2021-11-08 18:42:24 -0800

    Add hyperlink functionality to Svg Group

--- lib/genesis/utils/formats/svg/group.hpp
+++ lib/genesis/utils/formats/svg/group.hpp
@@ -3,7 +3,7 @@
 
 /*
     Genesis - A toolkit for working with phylogenetic data.
-    Copyright (C) 2014-2018 Lucas Czech and HITS gGmbH
+    Copyright (C) 2014-2021 Lucas Czech and HITS gGmbH
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -19,9 +19,9 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
     Contact:
-    Lucas Czech <lucas.czech@h-its.org>
-    Exelixis Lab, Heidelberg Institute for Theoretical Studies
-    Schloss-Wolfsbrunnenweg 35, D-69118 Heidelberg, Germany
+    Lucas Czech <lczech@carnegiescience.edu>
+    Department of Plant Biology, Carnegie Institution For Science
+    260 Panama Street, Stanford, CA 94305, USA
 */
 
 /**
@@ -37,6 +37,8 @@
 #include "genesis/utils/core/std.hpp"
 
 #include <iosfwd>
+#include <stdexcept>
+#include <unordered_map>
 
 namespace genesis {
 namespace utils {
@@ -94,6 +96,37 @@ public:
     self_type& reverse();
 
     /**
+     * @brief Simple tweak to allow making parts of an image clickable.
+     *
+     * This will simply add an `<a>` tag around the group, making everything in it clickable
+     * and hyperlink to the given URL. We currently do not have any per-object hyperlinks,
+     * and hence instead everything with a hyperlink has to go in a group.
+     */
+    self_type& set_hyperlink( std::string const& value )
+    {
+        hyperlink_ = std::unordered_map<std::string, std::string>{{ "href", value }};
+        return *this;
+    }
+
+    /**
+     * @brief Tweak to allow making parts of an image cickable.
+     *
+     * This is the same as the simply string version of this function, but additionally allows
+     * to set extra attributes to be used within the `<a>` tag. Note that the key `href` needs
+     * to be present in the map, in order to actually have a hyerlink to be cickable.
+     */
+    self_type& set_hyperlink( std::unordered_map<std::string, std::string> const& values )
+    {
+        if( values.count( "href" ) == 0 ) {
+            throw std::invalid_argument(
+                "Cannot set an SVG Group hyperlink without the `href` tag."
+            );
+        }
+        hyperlink_ = values;
+        return *this;
+    }
+
+    /**
      * @brief Shortcut operator for add(), which allows an even more fluent interface.
      */
     self_type& operator << ( SvgObject const& object );
@@ -109,6 +142,7 @@ public:
         id = "";
         content_.clear();
         transform.clear();
+        hyperlink_.clear();
     }
 
     // -------------------------------------------------------------
@@ -118,6 +152,7 @@ public:
     std::string              id;
     std::vector< SvgObject > content_;
     SvgTransform             transform;
+    std::unordered_map<std::string, std::string> hyperlink_;
 
 };
 
