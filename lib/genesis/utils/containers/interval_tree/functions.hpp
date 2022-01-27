fa1fa457ab072c995a487b0525ecab578fcec9e5
Lucas Czech <lucas.czech@h-its.org>
2022-01-26 20:12:39 -0800

    Add length function for Interval and IntervalTree

--- lib/genesis/utils/containers/interval_tree/functions.hpp
+++ lib/genesis/utils/containers/interval_tree/functions.hpp
@@ -3,7 +3,7 @@
 
 /*
     Genesis - A toolkit for working with phylogenetic data.
-    Copyright (C) 2014-2021 Lucas Czech
+    Copyright (C) 2014-2022 Lucas Czech
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -30,6 +30,8 @@
  * @file
  * @ingroup utils
  */
+
+#include <algorithm>
 
 #include "genesis/utils/containers/interval_tree/interval.hpp"
 
