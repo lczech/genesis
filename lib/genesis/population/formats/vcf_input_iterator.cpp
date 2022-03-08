b6921a1fb0b390d589019766d2da7dbefcd04feb
Lucas Czech <lucas.czech@h-its.org>
2022-03-08 10:23:38 -0800

    Minor fix

--- lib/genesis/population/formats/vcf_input_iterator.cpp
+++ lib/genesis/population/formats/vcf_input_iterator.cpp
@@ -1,6 +1,6 @@
 /*
     Genesis - A toolkit for working with phylogenetic data.
-    Copyright (C) 2014-2021 Lucas Czech
+    Copyright (C) 2014-2022 Lucas Czech
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -154,7 +154,7 @@ void VcfInputIterator::increment_()
 }
 
 void VcfInputIterator::fill_buffer_block_()
-{;
+{
     assert( thread_pool_ );
     assert( future_ );
 
