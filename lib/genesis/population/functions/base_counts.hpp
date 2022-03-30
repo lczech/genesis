b2527a758242797c22c8d84a44c04b9678278a3c
Lucas Czech <lucas.czech@h-its.org>
2022-03-29 21:40:43 -0700

    Remove left over function declaration

--- lib/genesis/population/functions/base_counts.hpp
+++ lib/genesis/population/functions/base_counts.hpp
@@ -239,11 +239,6 @@ std::pair<char, double> consensus( BaseCounts const& sample );
 */
 std::pair<char, double> consensus( BaseCounts const& sample, BaseCountsStatus const& status );
 
-/**
- * @brief Set the counts of a BaseCounts to zero.
- */
-void reset( BaseCounts& counts );
-
 // =================================================================================================
 //     Conversion Functions
 // =================================================================================================
