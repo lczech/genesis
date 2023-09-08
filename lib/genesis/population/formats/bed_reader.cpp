9965b77d5cf5b6be296209f78442951152e245ad
Lucas Czech <lucas.czech@h-its.org>
2023-09-07 23:12:25 -0700

    Fix for initializer list issue lczech/grenedalf#11

--- lib/genesis/population/formats/bed_reader.cpp
+++ lib/genesis/population/formats/bed_reader.cpp
@@ -1,6 +1,6 @@
 /*
     Genesis - A toolkit for working with phylogenetic data.
-    Copyright (C) 2014-2022 Lucas Czech
+    Copyright (C) 2014-2023 Lucas Czech
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -116,7 +116,7 @@ void BedReader::read_(
         }
         assert( found_columns == expected_columns );
         callback( std::move( feat ));
-        feat = Feature{};
+        feat = Feature();
     }
 }
 
