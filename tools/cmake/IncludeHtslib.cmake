277292d4a4b2a081ef7ca4a8e813fdeb8b19e272
Lucas Czech <lucas.czech@h-its.org>
2024-02-20 14:24:13 -0300

    Fix cmake clang htslib incompatibility, pt 8

--- tools/cmake/IncludeHtslib.cmake
+++ tools/cmake/IncludeHtslib.cmake
@@ -239,8 +239,7 @@ ExternalProject_Add(
         # cleanly implemented, see https://github.com/samtools/htslib/issues/1527
         # $(MAKE) CFLAGS+=${HTSLIB_COMPILER_FLAGS} CXXFLAGS+=${HTSLIB_COMPILER_FLAGS}
         # Nope... the above failes under clang?! Let's just deactivate for now.
-        # $(MAKE)
-        make
+        $(MAKE)
 
     # Install Step
     INSTALL_COMMAND $(MAKE) install
