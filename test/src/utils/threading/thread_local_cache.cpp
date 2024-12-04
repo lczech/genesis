096f03c6f420fa9df3eb8fc63bba27f60da5384a
Lucas Czech <luc.czech@gmail.com>
2024-12-04 18:36:48 +0100

    Fix test case for Thread Local Cache size

--- test/src/utils/threading/thread_local_cache.cpp
+++ test/src/utils/threading/thread_local_cache.cpp
@@ -47,28 +47,33 @@ TEST( ThreadPool, ThreadLocalCache )
     size_t const num_threads = 8;
     size_t const num_tasks = 10000;
 
-    // We here set up the thread pool with exactly the number of threads we want.
-    // In a real life scenario, we usually want one thread fewer, and limit the task queue
-    // (second argument of the pool), such that we get a pool with work stealing,
-    // so that the main thread can also do work while waiting. Not done here for simplicity.
-    auto global_cache = ThreadLocalCache<size_t>( num_threads );
-    auto thread_pool = ThreadPool( num_threads );
+    // We here set up the cache with one additional cache entry, which might be needed
+    // in case that the final waiting for pending tasks actually has work to do:
+    // Our Thread Pool implements work stealing, and so the main thread might then execute work,
+    // so that we better allocate a cache entry for its thread local use as well.
+    // Use brace initialization, as some older compilers (clang 6-8) do not yet expect copy elision,
+    // such that the explicitly deleted move constructor fails to compile here otherwise.
+    ThreadLocalCache<size_t> local_caches{ num_threads + 1 };
+    ThreadPool thread_pool{ num_threads };
 
     // Enqueue some tasks, here, just a simple sum of all numbers up to num_tasks.
+    // LOG_DBG << "start";
     for( size_t i = 0; i < num_tasks; ++i ) {
-        thread_pool.enqueue_detached( [i, &global_cache]()
+        thread_pool.enqueue_detached( [i, &local_caches]()
         {
-            thread_local auto& cache = global_cache.get();
-            cache += i;
+            thread_local auto& local_cache = local_caches.get();
+            local_cache += i;
         });
     }
     thread_pool.wait_for_all_pending_tasks();
+    // LOG_DBG << "finish";
 
     // Sum up the sums of all caches.
     size_t total = 0;
-    for( auto const& cache : global_cache ) {
-        total += cache;
+    for( auto const& local_cache : local_caches ) {
+        total += local_cache;
     }
+    // LOG_DBG << "Cache: " << local_caches.size() << "/" << local_caches.capacity();
 
     // Check that we have the correct result, i.e., the sum of all numbers smaller than num_tasks
     auto const exp_val = num_tasks * (num_tasks - 1) / 2;
