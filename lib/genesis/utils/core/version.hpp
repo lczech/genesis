6918840e24b18ebf7668f33872ef722c0ff1ec0c
Lucas Czech <luc.czech@gmail.com>
2024-12-17 20:42:47 +0100

    Release v0.33.0

--- lib/genesis/utils/core/version.hpp
+++ lib/genesis/utils/core/version.hpp
@@ -63,7 +63,7 @@ namespace genesis {
 inline std::string genesis_version()
 {
     // The following line is automatically replaced by the deploy scripts. Do not change manually.
-    return "v0.32.0"; // #GENESIS_VERSION#
+    return "v0.33.0"; // #GENESIS_VERSION#
 }
 
 /**
@@ -74,7 +74,7 @@ inline std::string genesis_version()
 inline std::string genesis_version_name()
 {
     // The following line is automatically replaced by the deploy scripts. Do not change manually.
-    return "Matter"; // #GENESIS_VERSION_NAME#
+    return "Phase Change"; // #GENESIS_VERSION_NAME#
 }
 
 /**
