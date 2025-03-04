fc014670b3ded5bc5134d8e46cfe642357b228e5
Lucas Czech <luc.czech@gmail.com>
2025-03-04 18:44:11 +0100

    Add Fai Input Stream

--- lib/genesis/sequence.hpp
+++ lib/genesis/sequence.hpp
@@ -3,7 +3,7 @@
 
 /*
     Genesis - A toolkit for working with phylogenetic data.
-    Copyright (C) 2014-2024 Lucas Czech
+    Copyright (C) 2014-2025 Lucas Czech
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -34,6 +34,7 @@
  */
 
 #include "genesis/sequence/counts.hpp"
+#include "genesis/sequence/formats/fai_input_stream.hpp"
 #include "genesis/sequence/formats/fasta_reader.hpp"
 #include "genesis/sequence/formats/fasta_writer.hpp"
 #include "genesis/sequence/formats/fastq_reader.hpp"
