845142e5c627ebda65cc9a84103e2c3baf3942b3
Lucas Czech <luc.czech@gmail.com>
2025-03-12 01:09:18 +0100

    Revert back to working solution for Kmer Color Set concurrency

--- lib/genesis/sequence/kmer/color_set.hpp
+++ lib/genesis/sequence/kmer/color_set.hpp
@@ -309,12 +309,10 @@ private:
     // shared/exclusive access for the color list and lookup, but also differenciate between
     // the color accumualtion phase and the gamut phase...
     // The mutex_ is used for shared/exclusive locking during the color collection phase,
-    // such that the color list and lookup can grow under concurrent access, while the atomic
-    // makes sure that a writer cannot starve.
+    // such that the color list and lookup can grow under concurrent access.
     // Then, once we have saturated the colors and switch to the gamut, we instead use a more
     // fine-grained locking of the gamut matrix cells instead, using the vector guard.
-    std::atomic<bool> write_pending_{false};
-    mutable std::shared_mutex mutex_;
+    mutable std::shared_timed_mutex mutex_;
     mutable utils::ConcurrentVectorGuard gamut_guard_;
 
 };
