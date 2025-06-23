1a27abc87aae7ff49a10296b86d147694ecff0fd
Lucas Czech <luc.czech@gmail.com>
2025-06-24 01:27:21 +0200

    Fix missing header include in test

--- test/src/utils/threading/sequential_output_buffer.cpp
+++ test/src/utils/threading/sequential_output_buffer.cpp
@@ -1,6 +1,6 @@
 /*
     Genesis - A toolkit for working with phylogenetic data.
-    Copyright (C) 2014-2024 Lucas Czech
+    Copyright (C) 2014-2025 Lucas Czech
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -33,6 +33,7 @@
 #include "genesis/utils/math/random.hpp"
 #include "genesis/utils/threading/sequential_output_buffer.hpp"
 
+#include <algorithm>
 #include <atomic>
 #include <future>
 #include <numeric>
