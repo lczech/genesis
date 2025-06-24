0bb4423fb26c1e56ac3afd33084c5a76ae5c7221
Lucas Czech <luc.czech@gmail.com>
2025-06-24 16:13:42 +0200

    Upgrade xkcd version getter to Python3

--- tools/deploy/xkcd.py
+++ tools/deploy/xkcd.py
@@ -1,12 +1,14 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 import json
 import urllib
+import urllib.request
 
 # Get latest xkcd data
-url = "http://xkcd.com/info.0.json"
-response = urllib.urlopen(url)
-data = json.loads(response.read())
+xkcd_url = "http://xkcd.com/info.0.json"
+with urllib.request.urlopen(xkcd_url) as url:
+    response = url.read()
+    data = json.loads(response)
 
 # Print the title, with proper escaping
-print json.dumps(data["safe_title"]).strip('"')
+print(json.dumps(data["safe_title"]).strip('"'))
